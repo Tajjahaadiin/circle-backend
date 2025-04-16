aaf920695cbfd32b7c0267680d6fee618bc773b3
Tajjuddin <tajjuddiinauliya@gmail.com>
2025-04-16 21:42:41 +0700

    change cors

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import { errorHandler } from './middleware/error.middleware';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: ['https://foundation-circle.vercel.app/login'] }));
 
 const port = process.env.PORT;
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc, swaggerConfig));
